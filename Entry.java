4dc314c0110c164e176caa3bd93d6ea417ff267c
唐靖钧 <1195669260@qq.com>
2020-03-12 14:12:25 +0800

    新增获取文件中各行人数的方法

--- Entry.java
+++ Entry.java
@@ -1,6 +1,10 @@
 package entry;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Vector;
 
 public class Entry {
@@ -11,9 +15,51 @@ public class Entry {
 //		System.out.println("Yes");
 	}
 	
+	/*
+	 * 处理每一个文件
+	 */
 	private static void solveTheFile(String[] fileList) {
 		// TODO Auto-generated method stub
-		
+		for(String aFile : fileList) {
+			String fileDate = aFile.substring(0, aFile.indexOf("."));
+			
+			try {
+	    		File file = new File(path + "\\" + aFile);
+	    		InputStreamReader inputReader = new InputStreamReader(new FileInputStream(file), "UTF-8");
+	    		BufferedReader bf = new BufferedReader(inputReader);
+	    		String str;
+	    		
+	    		while ((str = bf.readLine()) != null && str.indexOf("//") != 0) {
+	    			//System.out.println(str);
+	    			String[] information = str.split("\\s+");
+	    			//System.out.println(information[0]);
+	    			String province = information[0];//先取到省份
+	    			int number = getNumber(information);//取出各行人数
+	    			
+	    			
+	    		}			
+	    		bf.close();		
+	    	//	bw.close();
+	    		inputReader.close();
+	    	
+			} 
+	    	catch (IOException  e) {
+	    		e.printStackTrace();
+			}
+//			System.out.println(fileDate);
+		}
+	}
+	
+	/*
+	 * 获取各行的人数
+	 */
+	public static int getNumber(String[] information) {
+		//获取人数
+		String numString = information[information.length - 1];
+		int index = numString.indexOf("人");
+		numString = numString.substring(0, index);
+		int number = Integer.parseInt(numString);
+		return number;
 	}
 
 	/*
