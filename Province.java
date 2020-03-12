114ac208eccc7b3344a33a69c51f54859b70a741
唐靖钧 <1195669260@qq.com>
2020-03-12 11:35:19 +0800

    更新Province类方法

--- Province.java
+++ Province.java
@@ -1,5 +1,24 @@
 package edu.fzu.InfectStatisticWeb.pojo;
 
-public class Province {
+import java.util.List;
 
+public class Province {
+	private String name;
+	private List<Date> dateList;
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public List<Date> getDateList(){
+		return dateList;
+	}
+	
+	public void addDate(Date d) {
+		dateList.add(d);
+	}
 }
