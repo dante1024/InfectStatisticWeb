5a05c870841ff4a65a0514bc6c293fcb033b50ec
唐靖钧 <1195669260@qq.com>
2020-03-12 14:49:30 +0800

    新增初始化map(存储全国各地区信息)方法

--- Entry.java
+++ Entry.java
@@ -5,16 +5,35 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Vector;
+
+import edu.fzu.InfectStatisticWeb.pojo.Province;
 
 public class Entry {
 	private static String path = "C:\\Users\\Peter\\Desktop\\部分疫情日志log";
+	public static Map<String, Province> map = new HashMap<String, Province>();
+	public static String[] province = {"全国", "安徽", "北京", "重庆", "福建", "甘肃", "广东", "广西", "贵州", "海南", "河北",
+			"河南", "黑龙江", "湖北", "湖南", "吉林", "江苏", "江西", "辽宁", "内蒙古", "宁夏", "青海", "山东", "山西", "陕西",
+			"上海", "四川", "天津", "西藏", "新疆", "云南", "浙江"
+	};
 	public static void main(String[] args) {
+		initMap();
 		String[] fileList = getAllFile(path);
 		solveTheFile(fileList);
 //		System.out.println("Yes");
 	}
 	
+	private static void initMap() {
+		// TODO Auto-generated method stub
+		for(String aProvince : province) {
+			Province p = new Province();
+			p.setName(aProvince);
+			map.put(aProvince, p);
+		}
+	}
+
 	/*
 	 * 处理每一个文件
 	 */
@@ -40,7 +59,9 @@ public class Entry {
 	    			int number = getNumber(information);//取出各行人数
 	    			//System.out.println(number);
 	    			
-	    			
+	    			if(map.get(province) == null) {
+	    				Province p = 
+	    			}
 	    		}			
 	    		bf.close();		
 	    	//	bw.close();
